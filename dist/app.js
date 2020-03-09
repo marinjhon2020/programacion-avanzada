b13bd39e1f2030312e7dda40e31763aad97f6a64
Marinjhon <marinjhon2019@gmail.com>
2020-03-09 11:38:09 -0500

    programacion-avansada

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-
-import express from 'express';
-import router from './Routes/ProjectRoutes';
-
-let app = express();
-app.use(express.urlencoded({extended:false}));
-app.use('/api',router);
-module.exports = app;
